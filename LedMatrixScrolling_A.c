0d2dbebefd027a14f8880d682da866bfa2191cdd
Pankaj Kumar <pankaj200497@gmail.com>
2018-06-12 19:20:50 +0530

    Update LedMatrixScrolling_A.c

--- LedMatrixScrolling_A.c
+++ LedMatrixScrolling_A.c
@@ -7,14 +7,17 @@ unsigned char row[8]= { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
 void a()
 {
  unsigned char clm[8]= {0xBD,0xBD,0xBD,0x81,0xBD,0xBD,0xDB,0xE7};
- for(int k=0;k<8;k++){
+ for(int k=0;k<8;k++)
+ {
   for (int j=0; j<500; j++)
-  {  for(int i=0; i<8; i++)
-      {PORTB= row[i];
-	   PORTA= ~((~clm[i])<<k);
-	   PORTA|= ~(255<<k);
-	   _delay_ms(1);}
-   }
+  {  
+    for(int i=0; i<8; i++)
+      { 
+	  PORTB= row[i];
+	  PORTA= ~((~clm[i])<<k);
+	  PORTA|= ~(255<<k);
+	  _delay_ms(1);}
+       }
    }
    PORTB= 0b00000000;
    PORTA= 0b11111111;
